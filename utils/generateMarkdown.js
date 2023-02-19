a47ddd4f049df3a605cc81acb38e4b9874dd59d9
Daniel Ferreira <daniel_jferreira@hotmail.com>
2023-02-19 20:12:16 +0000

    implement the gerneateMarkdown arrow function to generate each element for the readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,32 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+const generateMarkdown = (data) =>{
+  return `
+#${data.title} 💡
 
-`;
+## Description
+📝 ${data.description}
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Credits](#credits)
+- [Tests](#tests)
+## Installation
+💾 ${data.installation}
+## Usage
+🎯 ${data.usage}
+## License
+![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)
+<br />
+This application is covered by the ${data.license} license. 
+## Credits
+🤝 ${data.contributing}
+## Tests
+🚦${data.tests}
+<br />
+ This Project was created by [My_README_Generator](https://github.com/DaniJFerr/My_README_Generator) 
+    `;
 }
-
+  
 module.exports = generateMarkdown;
